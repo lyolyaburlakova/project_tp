e90a330fdb868c280d9c667e110f1db3f45b095f
lyolyaburlakova <90637037+lyolyaburlakova@users.noreply.github.com>
2022-04-02 01:22:38 +0300

    добавлены файлы и uml

---
+++ battleship/main.java
@@ -0,0 +1,13 @@
+package battleship;
+
+public class main {
+    public static void main(String[] args) {
+        int size = 10;
+        field field1 = new field(size);
+
+       //field.drawfield();
+       //player.shoot();
+       //player.put_ships();
+
+    }
+}
---
+++ battleship/ship.java
@@ -0,0 +1,12 @@
+package battleship;
+
+public class ship {
+    public int[] coordinates;
+    public int size;
+
+    public ship(int size, int[] coordinates){
+        this.size = size;
+        this.coordinates = new int[10];
+
+    }
+}
